5cf39cc258425cd9556f307997b132704866ac40
ScriptKiddie47 <ritambala@live.com>
2021-10-08 21:06:28 +0530

    Add MatchPage,New Rest Call

--- src/ipl-frontend/src/pages/matchPage.js
+++ src/ipl-frontend/src/pages/matchPage.js
@@ -1,7 +1,31 @@
+import { React, useEffect, useState } from 'react'
+import { useParams } from 'react-router';
+import { MatchDetailedCard } from '../components/MatchDetailCard';
+import { MatchSmallCard } from '../components/MatchSmallCard';
+
 export const MatchPage = () => {
+
+    const [matches, setMatches] = useState([])
+    const { teamName, year } = useParams() //Fetched from route call
+    useEffect(
+        () => {
+            const fetchMatches = async () => {
+                const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+                const data = await response.json();
+                setMatches(data);
+            };
+            fetchMatches();
+        }, []
+    );
+
+
     return (
         <div className="MatchPage">
             <h1>MatchPage</h1>
+            {
+                matches.map(match =>
+                    <MatchDetailedCard teamName={teamName} match={match} />)
+            }
         </div>
     );
 }
--- src/main/java/com/scriptkiddie/ipldashboard/controller/TeamControllerImpl.java
+++ src/main/java/com/scriptkiddie/ipldashboard/controller/TeamControllerImpl.java
@@ -1,11 +1,14 @@
 package com.scriptkiddie.ipldashboard.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.scriptkiddie.ipldashboard.model.Team;
@@ -26,5 +29,13 @@ public class TeamControllerImpl {
 		Team team = teamRepository.findByTeamName(teamName);
 		team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4 ));
 		return ResponseEntity.ok(team);
+	}
+	
+	@RequestMapping(value = "/team/{teamName}/matches", method = RequestMethod.GET)
+	public ResponseEntity<?> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+		LocalDate dateStart = LocalDate.of(year, 1, 1);
+		LocalDate dateEnd = LocalDate.of(year + 1, 1, 1);
+		return ResponseEntity.ok(
+				matchRepository.getMatchesByTeamBetweenDates(teamName, dateStart, dateEnd));
 	}
 }
